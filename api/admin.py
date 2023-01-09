557df4b01452762d9f2a3a4b70bc8422ea5f457e
Andrew Crossan <andrew.crossan23@outlook.com>
2023-01-09 00:20:27 +0000

    Register Product, Category and Brand

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+# Shop Administration
+from .models import Product, Category, Brand
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'Price', 'Stock', 'Available', 'Updated', 'category_name', 'brand_name')
+    list_filter = ('Available', 'Created', 'Updated')
+    list_editable = ('Price', 'Stock', 'Available')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'product_count', 'average_rating')
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('Name', 'product_count')
+    list_filter = ('Name',)
