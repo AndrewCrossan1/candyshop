f7da286f3097732364e1e5e9f14b08c814cfe0c6
Andrew Crossan <andrew.crossan23@outlook.com>
2023-01-09 00:33:31 +0000

    Changed ProductTests to CategoryTests

--- api/tests.py
+++ api/tests.py
@@ -1,15 +1,26 @@
 from django.test import TestCase
 
+from api.models import Category
+
 
 # Shop Tests
-
-class ProductTests(TestCase):
+class CategoryTests(TestCase):
     # Test Product Model
     def setUp(self):
-        pass
-    def test_product_creation(self):
-    # Delete the product
-        pass
+        Category.objects.create(Name="Test Category",
+                                Description="Test Description")
 
-    def test_product_deletion(self):
-        pass
+    def test_category_creation(self):
+        # Test the product
+        category = Category.objects.get(Name="Test Category")
+        self.assertEqual(category.Name, "Test Category")
+
+    def test_category_count(self):
+        # Test the product count
+        self.assertEqual(Category.objects.count(), 1)
+
+    def test_category_deletion(self):
+        # Delete the product
+        category = Category.objects.get(Name="Test Category")
+        category.delete()
+        self.assertEqual(Category.objects.count(), 0)
