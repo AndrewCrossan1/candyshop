eefe9a71241b1ae81003ce7811ff4455aafd64a9
Andrew Crossan <andrew.crossan23@outlook.com>
2023-01-09 00:22:51 +0000

    Added initial views for new models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Product, Category, Brand
+from .serializers import ProductSerializer, CategorySerializer, BrandSerializer
+
+
+# Shop Views
+class ProductViewSet(viewsets.ModelViewSet):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class BrandViewSet(viewsets.ModelViewSet):
+    permission_classes = []
+    authentication_classes = []
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
