79a87979586363ac08cc887323d3485499da87f0
AndrewCrossan1 <andrew.crossan23@outlook.com>
2023-01-11 00:38:22 +0000

    Added respective permissions for each viewset

--- api/views.py
+++ api/views.py
@@ -1,27 +1,27 @@
-from django.shortcuts import render
-from rest_framework import viewsets
-
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAdminUser
 from .models import Product, Category, Brand
+from .permissions import ProductPermission, CategoryPermission, BrandPermission
 from .serializers import ProductSerializer, CategorySerializer, BrandSerializer
 
 
 # Shop Views
-class ProductViewSet(viewsets.ModelViewSet):
-    permission_classes = []
+class ProductViewSet(ModelViewSet):
+    permission_classes = [ProductPermission]
     authentication_classes = []
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
-    permission_classes = []
+class CategoryViewSet(ModelViewSet):
+    permission_classes = [CategoryPermission]
     authentication_classes = []
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class BrandViewSet(viewsets.ModelViewSet):
-    permission_classes = []
+class BrandViewSet(ModelViewSet):
+    permission_classes = [BrandPermission]
     authentication_classes = []
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
