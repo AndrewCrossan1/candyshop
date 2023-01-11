775e454ae15c556ede8a2cca0102aa90f7829a62
Andrew Crossan <andrew.crossan23@outlook.com>
2023-01-11 01:43:44 +0000

    Removed redundant import

--- api/models.py
+++ api/models.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.utils.html import format_html
 from taggit.managers import TaggableManager
